1b5df204ecf9a27db9a582045d66d5b9dcb52995
Jenny Wu <jennywu22553@gmail.com>
2025-04-12 11:38:53 -0500

    Added the initializeCandidates method that initializes the election system with the specified list of candidates given

--- src/Election.java
+++ src/Election.java
@@ -1,2 +1,14 @@
+import java.util.*;
 public class Election {
+    private Map<String, Integer> candidatesMap;
+    private PriorityQueue<Map.Entry<String, Integer>> maxHeap;
+    private int totalVotes;
+    public void initializeCandidates(LinkedList<String> candidates, int p) {
+        candidatesMap = new HashMap<>();
+        for (String candidate : candidates) {
+            candidatesMap.put(candidate, 0);
+        }
+        maxHeap = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        totalVotes = p;
+    }
 }
