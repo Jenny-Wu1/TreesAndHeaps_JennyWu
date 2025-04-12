e7e77058fed9eb86e02211d0013496eac5d6c4d3
Jenny Wu <jennywu22553@gmail.com>
2025-04-12 11:47:02 -0500

    Implemented the ElectionSystem class that performs various operations on a created Election instance like initializing the number of people that can vote, casting p votes, rigging the election, and getting the top k candidates

--- src/ElectionSystem.java
+++ src/ElectionSystem.java
@@ -1,2 +1,23 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class ElectionSystem {
+    public static void main(String[] args) {
+        Election election = new Election();
+        LinkedList<String> candidates = new LinkedList<>(Arrays.asList(
+                "Marcus Fenix", "Dominic Santiago", "Damon Baird", "Cole Train", "Anya Stroud"
+        ));
+        int p = 5;
+        
+        election.initializeCandidates(candidates, p);
+        election.castVote("Cole Train");
+        election.castVote("Cole Train");
+        election.castVote("Marcus Fenix");
+        election.castVote("Anya Stroud");
+        election.castVote("Anya Stroud");
+        System.out.println("Top 3 candidates after 5 votes: " + election.getTopKCandidates(3));
+        election.rigElection("Marcus Fenix");
+        System.out.println("Top 3 candidates after rigging the election: " + election.getTopKCandidates(3));
+        election.auditElection();
+    }
 }
